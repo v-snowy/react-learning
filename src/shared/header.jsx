5dcffdaba98ed234c8b2f143a3d139325317613e
Vasiliy Snegovoy <vasiliy.snegovoy@gmail.com>
2021-10-21 22:16:12 +0400

    Added styles for header

--- src/shared/header.jsx
+++ src/shared/header.jsx
@@ -1,11 +1,12 @@
 import { hot } from 'react-hot-loader/root';
 import React from 'react';
 import { Example } from './example';
+import styles from './header.css';
 
 function HeaderComponent() {
   return (
     <header>
-      <h1>Reddit for our own</h1>
+      <h1 className={ styles.header }>Reddit for our own</h1>
       <Example/>
     </header>
   );
