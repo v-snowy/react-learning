770c2012ae4671d3600c48574dd657c87146e00f
Vasiliy Snegovoy <vasiliy.snegovoy@gmail.com>
2022-02-26 16:38:53 +0400

    FIxed port

--- src/server/server.js
+++ src/server/server.js
@@ -4,6 +4,7 @@ import ReactDOM from 'react-dom/server';
 import { App } from '../App';
 import { indexTemplate } from './indexTemplate';
 
+const PORT = process.env.PORT || 3000;
 const app = express();
 
 app.use('/static', express.static('./dist/client'));
@@ -31,6 +32,6 @@ app.get('*', (req, res) => {
   );
 });
 
-app.listen(3000, () => {
-  console.log('Server has been started on http://localhost:3000');
+app.listen(PORT, () => {
+  console.log(`Server has been started on http://localhost:${ PORT });
 })
